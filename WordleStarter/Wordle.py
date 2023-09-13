5dac7f7fc97c8d6a9ccce298f3ee8f704a07d3ce
raygenblackwell <raygen.m.black@gmail.com>
2023-09-12 18:55:14 -0600

    comments added

--- WordleStarter/Wordle.py
+++ WordleStarter/Wordle.py
@@ -10,20 +10,20 @@ import random
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
-CORRECT_COLOR = "#66BB66" # A shade of green
-PRESENT_COLOR = "#CCBB66" # A shade of brownish yellow
-MISSING_COLOR = "#999999" # A shade of gray
+CORRECT_COLOR = "#66BB66"  # A shade of green
+PRESENT_COLOR = "#CCBB66"  # A shade of brownish yellow
+MISSING_COLOR = "#999999"  # A shade of gray
+
 
 def wordle():
     gw = WordleGWindow()
-    
+
     # Select a random secret word from the list
     secret_word = random.choice(FIVE_LETTER_WORDS)
     # Display secret word for testing purposes
-    for i in range (len(secret_word)):
-        gw.set_square_letter(0,i-1,secret_word[i-1].upper())
+    for i in range(len(secret_word)):
+        gw.set_square_letter(0, i-1, secret_word[i-1].upper())
 
-    
     def enter_action(s):
         guess = s[:N_COLS]
         # Check to see if the word is in the word list
@@ -34,23 +34,25 @@ def wordle():
             for i in range(len(secret_word)):
                 for j in range(len(secret_word)):
                     # If the letter is correct and in the correct position
-                    if (secret_word[i-1].lower() == guess[j-1].lower() and i==j):
-                        gw.set_square_color(gw.get_current_row(),i-1,CORRECT_COLOR)
+                    if (secret_word[i-1].lower() == guess[j-1].lower() and i == j):
+                        gw.set_square_color(
+                            gw.get_current_row(), i-1, CORRECT_COLOR)
                     # If the letter is correct but in the wrong position
                     # elif (guess[j-1].lower() in secret_word and i != j):
                     #     gw.set_square_color(gw.get_current_row(),i-1,PRESENT_COLOR)
-            
+
             # Move down to the next row
             rowNum = gw.get_current_row()
-            if rowNum < N_ROWS -1:
+            if rowNum < N_ROWS - 1:
                 gw.set_current_row(rowNum + 1)
         else:
             gw.show_message("Not in word list")
-        
+
         # gw.add_enter_listener(enter_action)
     gw.add_enter_listener(enter_action)
 
 # Startup code
+
 
 if __name__ == "__main__":
     wordle()
